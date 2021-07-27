f7cd9652869aa8fb4028a4405d3007297c0d067c
jnasser513 <00245818@uca.edu.sv>
2021-07-27 14:24:28 -0600

    Setting up the store recycler adapter

--- Stores App/app/src/main/java/com/nasser/storesapp/StoreAdapter.kt
+++ Stores App/app/src/main/java/com/nasser/storesapp/StoreAdapter.kt
@@ -1,13 +1,48 @@
 package com.nasser.storesapp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.nasser.storesapp.classes.Store
 import com.nasser.storesapp.databinding.ItemStoreBinding
 
-class StoreAdapter {
+class StoreAdapter(private var storeList: MutableList<Store>, private var listener: OnClickListener):
+    RecyclerView.Adapter<StoreAdapter.ViewHolder>(){
+
+    private lateinit var mContext: Context
+
+    //Creacion de la vista para mostrar el item_store
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        mContext = parent.context
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_store, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    //Visualizacion de las tiendas existentes en la base de datos
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val store = storeList.get(position)
+
+        with(holder) {
+            setListener(store)
+
+            binding.textViewTittle.text = store.name
+        }
+    }
+
+    override fun getItemCount(): Int = storeList.size
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(store: Store){
+            binding.root.setOnClickListener {
+                listener.onClick(store)
+            }
+        }
     }
 
 }
